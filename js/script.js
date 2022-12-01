71eaa87db463f3b7789656683523998d2fb60e92
graphiz-code <graphiz.graphiz@gmail.com>
2022-12-01 17:19:23 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+    /*** FUNCTION OUVRIR ET FERMER FORMULAIRE PRODUIT *****/
+    
+    function ouvrirProduitA() {
+    document.getElementById("monProduitA").style.display ="block";
+    }
+    function fermerProduitA() {
+    document.getElementById("monProduitA").style.display = "none";
+    }
+    function ouvrirProduitB() {
+    document.getElementById("monProduitB").style.display ="block";
+    }
+    function fermerProduitB() {
+    document.getElementById("monProduitB").style.display = "none";
+    }
+    function ouvrirProduitC() {
+    document.getElementById("monProduitC").style.display ="block";
+    }
+    function fermerProduitC() {
+    document.getElementById("monProduitC").style.display = "none";
+    }
+
+  /***********************/
+
+   
